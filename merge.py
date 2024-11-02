2663de5d44b2492b4093b9799ecfcb56bbdf604c
tom851024 <karta2066274@gmail.com>
2024-11-02 16:21:57 +0800

    feat:兩張照片合成功能
    已經完成將兩張照片按照指定點合在一起的功能，但是會有殘影

--- merge.py
+++ merge.py
@@ -29,8 +29,51 @@ def get_coordinates(event, x, y, flags, param):
                 # 結束並關閉視窗
                 print("第一張圖片的三個點:", points_img1)
                 print("第二張圖片的三個點:", points_img2)
+
+                # 計算 affine_matrix
+                affine_matrix = calculate_affine_transform(points_img2, points_img1)
+                print("Affine Matrix:\n", affine_matrix)
+
+                # 計算畫布大小
+                height, width, _ = img1.shape
+                output_canvas = np.zeros((height * 2, width * 2, 3), dtype=np.uint8)
+
+                # 將目標圖像放在畫布的中心
+                output_canvas[0:height, 0:width] = img1
+
+                # 將來源圖像應用仿射變換並放置在畫布上
+                warped_src_img = cv2.warpAffine(img2, affine_matrix, (width * 2, height * 2))
+                output_canvas = cv2.addWeighted(output_canvas, 1, warped_src_img, 1, 0)
+
+                # 顯示結果
+                cv2.imshow("Merged Image", output_canvas)
+                cv2.waitKey(0)
                 cv2.destroyAllWindows()
                 exit()
+
+def calculate_affine_transform(src_points, dst_points):
+    A = []
+    B = []
+    for i in range(3):
+        x_src, y_src = src_points[i]
+        x_dst, y_dst = dst_points[i]
+        A.append([x_src, y_src, 1, 0, 0, 0])
+        A.append([0, 0, 0, x_src, y_src, 1])
+        B.append(x_dst)
+        B.append(y_dst)
+
+    # 求解方程組以獲取仿射變換參數
+    A = np.array(A)
+    B = np.array(B)
+    affine_params = np.linalg.lstsq(A, B, rcond=None)[0]
+
+    # 組裝成 2x3 仿射矩陣
+    affine_matrix = np.array([
+        [affine_params[0], affine_params[1], affine_params[2]],
+        [affine_params[3], affine_params[4], affine_params[5]]
+    ])
+    return affine_matrix
+
 
 click_count = 0
 current_image = 1
